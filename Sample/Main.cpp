a65118bb594119d29c39473695293f302f4bcd93
Joda <johan.lore@free.fr>
2012-09-21 17:54:46 +0200

    Mise a jour des librairies + implémentation de l'engins

---
+++ Sample/Main.cpp
@@ -0,0 +1,26 @@
+#include <OgreException.h>
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT ) {
+#else
+int main( int argc, char **argv ) {
+#endif
+    GameManager *gameManager = GameManager::getSingletonPtr();
+
+    try {
+        // Initialise the game and switch to the first state
+        gameManager->startGame( IntroState::getSingletonPtr() );
+    }
+    catch ( Ogre::Exception& ex ) {
+        #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+                MessageBox( NULL, ex.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL );
+        #else
+                std::cerr << "An exception has occured: " << ex.getFullDescription();
+        #endif
+    }
+
+    delete gameManager;
+    return 0;
+}
