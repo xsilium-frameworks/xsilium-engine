12d558db51af91b4c41b0dc5771c53a7088e23ee
Pierre-Pascal Lapensee <xelfes@gmail.com>
2012-10-07 09:18:14 -0400

    Creation de la classe Console

--- Sample/GameState.h
+++ Sample/GameState.h
@@ -5,6 +5,8 @@
 
 #include "DotSceneLoader.h"
 
+#include"Console/Console.h"
+
 #include <OgreSubEntity.h>
 #include <OgreMaterialManager.h>
 
@@ -74,33 +76,9 @@ protected:
 
     // convert an OIS mouse button into a CEGUI mouse button
     CEGUI::MouseButton convertOISButtonToCegui(int buttonID);
-//    Console* d_console;
-};
-
-class Console
-{
-public:
-    Console(CEGUI::Window* parent = 0);
-    ~Console();
-
-    void toggleVisibility();
-    bool isVisible() const;
-
-private:
-    // these must match the IDs assigned in the layout
-    static const unsigned int SubmitButtonID;
-    static const unsigned int EntryBoxID;
-    static const unsigned int HistoryID;
-
-    bool handleSubmit(const CEGUI::EventArgs& args);
-    bool handleKeyDown(const CEGUI::EventArgs& args);
-
-    CEGUI::Window* d_root;
-    int d_historyPos;
-    std::vector<CEGUI::String> d_history;
-
 
 };
+
 
 
 #endif
