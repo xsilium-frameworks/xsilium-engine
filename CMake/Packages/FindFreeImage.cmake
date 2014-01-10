3657f548961fad7abc33322567f30f572c240491
Xelfe <xelfes@gmail.com>
2014-01-09 21:50:05 -0500

    - EN COURS - task references #211: Mise à jour de Ogre vers v1-9.0 stable - Correction FindFreeImage 3

--- CMake/Packages/FindFreeImage.cmake
+++ CMake/Packages/FindFreeImage.cmake
@@ -40,8 +40,8 @@ findpkg_framework(FreeImage)
 
 find_path(FreeImage_INCLUDE_DIR NAMES FreeImage.h HINTS ${XSILIUM_FREEIMAGE_INCLUDE} ${FreeImage_INC_SEARCH_PATH} ${FreeImage_PKGC_INCLUDE_DIRS})
 
-find_library(FreeImage_LIBRARY_REL NAMES ${FreeImage_LIBRARY_NAMES} HINTS ${FreeImage_LIB_SEARCH_PATH} ${FreeImage_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" release relwithdebinfo minsizerel)
-find_library(FreeImage_LIBRARY_DBG NAMES ${FreeImage_LIBRARY_NAMES_DBG} HINTS ${FreeImage_LIB_SEARCH_PATH} ${FreeImage_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" debug)
+find_library(FreeImage_LIBRARY_REL NAMES ${FreeImage_LIBRARY_NAMES} HINTS ${XSILIUM_FREEIMAGE_TARGET} ${FreeImage_LIB_SEARCH_PATH} ${FreeImage_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" release relwithdebinfo minsizerel)
+find_library(FreeImage_LIBRARY_DBG NAMES ${FreeImage_LIBRARY_NAMES_DBG} HINTS ${XSILIUM_FREEIMAGE_TARGET} ${FreeImage_LIB_SEARCH_PATH} ${FreeImage_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" debug)
 
 make_library_set(FreeImage_LIBRARY)
 
