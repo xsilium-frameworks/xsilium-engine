c89fa062e9d97728c8cfc12c650628ff888da055
xelfe <xelfes@gmail.com>
2014-03-24 22:15:31 -0400

    Modification outils SceneLoader

--- SceneLoader/SceneLoader.h
+++ SceneLoader/SceneLoader.h
@@ -1,20 +1,8 @@
-
 #ifndef __SceneLoader_h_
 #define __SceneLoader_h_
 
 #include "BaseApplication.h"
-#include <Terrain/OgreTerrain.h>
-#include <Terrain/OgreTerrainGroup.h>
-#include <OgreImage.h>
-
-class DotSceneLoader;
-
-namespace Forests
-{
-    class PagedGeometry;
-    class GrassLoader;
-    class GrassLayer;
-}
+#include "DotSceneLoader.h"
 
 class SceneLoader : public BaseApplication
 {
@@ -23,27 +11,7 @@ public:
     virtual ~SceneLoader(void);
 
 protected:
-    virtual void setupResources();
     virtual void createScene(void);
-    virtual bool keyPressed( const OIS::KeyEvent &arg );
-    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
-private:
-    std::vector<Ogre::String> mCamNames;
-    void switchCamera(int idx);
-    Ogre::TerrainGroup* mTerrainGroup;
-    DotSceneLoader* mLoader;
-    bool mTerrainImported;
-    Ogre::String mSceneFile;
-    Ogre::String mHelpInfo;
-    bool mFly;
-    Ogre::Real mFallVelocity;
-
-    Forests::PagedGeometry* mPGHandle;                         /** Handle to Forests::PagedGeometry object */
-    Forests::GrassLoader* mGrassLoaderHandle;                /** Handle to Forests::GrassLoader object */
-    Forests::GrassLayer* mPGLayers[4];
-    float* mPGLayerData[4];
-    Ogre::Image mPGDensityMap;
-    Ogre::Rect mPGDirtyRect;
 };
 
 #endif // #ifndef __SceneLoader_h_
